e61885f73ec5d31ff020270f28682f914135498a
anghelusjr <anghelusjr09.ab@gmail.com>
2025-11-07 00:31:33 +0800

    updated the blogs

--- src/pages/homePage/blog/blogData.jsx
+++ src/pages/homePage/blog/blogData.jsx
@@ -1,14 +1,15 @@
 
+
 export const blogData = [ 
     {
         id: "blog-1",
-        image: "/angel-buenavista-web-designer-hero-1240.webp",
+        image: "/angel-photos/angel-designer.webp",
         heading: "How My Journey Into Web Design Began",
         description: "My interest in Web Design  started when I explored how websites come together, and I quickly realized I wanted to build those experiences myself. Although I’m still developing my skills, I’ve worked on several projects, helping small businesses and personal brands establish their digital presence while continuing to grow as a designer. "
     },
     {
         id: "blog-2",
-        image: "/angel-buenavista-web-designer-hero-1240.webp",
+        image: "/angel-photos/team1.webp",
         heading: "Beyond the Screen",
         description: "I believe that great design is built not only through skills but also through collaboration. During our year-end gathering and costume party, I had the chance to connect with my teammates outside work. I even received the Best Cowboy Costume award, a fun reminder that I value creativity and building positive relationships in every setting."
     },
