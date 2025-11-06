b9fa1745e888332c82d951b46b718c4ef011b442
anghelusjr <anghelusjr09.ab@gmail.com>
2025-11-06 22:42:29 +0800

    update testimonials

--- src/pages/homePage/testimonial/testimonialData.jsx
+++ src/pages/homePage/testimonial/testimonialData.jsx
@@ -1,51 +1,51 @@
 export const testimonialData = [
   {
-    name: "Muñoz Jerwin",
+    name: "Jerwin M.",
     description:
       "Working with Angel Buenavista has been an exceptional experience. His creativity, attention to detail, and strong grasp of modern web design principles consistently bring our projects to life. Angel has a great sense of balance between aesthetics and functionality — ensuring every website he designs not only looks stunning but also delivers a seamless user experience. Highly recommended for anyone looking for a designer who can translate ideas into visually engaging and effective web designs.",
     relation: "Supervisor",
   },
   {
-    name: "Calope Jason",
+    name: "Jason C.",
     description:
       "I had the pleasure of working with Angel as my teammate for almost four years. He’s a great person to work with—friendly, reliable, and very detail-oriented. One thing that really stands out about him is his strong focus on accuracy and clarity. Whenever there’s a missing or confusing detail in a project, he never hesitates to ask questions to make sure everything is clear. He also consistently shares any issues or challenges he encounters so that the rest of the team can avoid the same problems. Overall, Angel is a thoughtful and dedicated web designer who truly cares about the quality of his work and the success of the team.",
     relation: "Colleague",
   },
   {
-    name: " Diaz Ed Joshua",
+    name: "Ed Joshua D.",
     description:
       "Angel is a straightforward and dependable team member. He actively contributes ideas, offers support to colleagues, and is always open to feedback and improvement. He’s one of the co-workers I can truly rely on.",
     relation: "Colleague",
   },
   {
-    name: "Libiran Angeline",
+    name: "Angeline L.",
     description:
       "Angel demonstrates strong teamwork and performs effectively even under pressure. He builds positive relationships with team members and consistently delivers high-quality designs.",
     relation: "Colleague",
   },
   {
-    name: "Mangarin Jhonrey",
+    name: "Jhonrey M.",
     description:
       "Angel is a hardworking and dedicated colleague who always puts in the effort to excel in his job. He consistently goes above and beyond what is expected of him. It is clear that Angel is passionate about his work and is committed to achieving success in his career.",
     relation: "Colleague",
   },
   {
-    name: "Floria Joseph",
+    name: "Joseph F.",
     description: "I’ve enjoyed working with Angel and calling him a friend. He’s an amazing web designer who truly cares about his work. He built a website that was not only beautiful but perfectly captured everything. Always professional, reliable, and incredibly detail-oriented. You’re in great hands with Angel!",
     relation: "Colleague",
   },
   {
-    name: "Condino Anthony",
+    name: "Anthony C.",
     description: "Angel Buenavista is such a joyful and friendly person to work with! He’s always willing to help and makes every collaboration enjoyable. When it comes to design, he truly has an eye for detail and creativity. His work consistently turns out impressive and well thought out. Highly recommend him for any design-related projects!",
     relation: "Colleague",
   },
    {
-    name: "Matt Paredes",
+    name: "Matt P.",
     description: "Angel is my cousin and we basically grew up together. In all honesty, I can confidently say he is a great individual. A hardworking, dedicated, and just a delight to be around with. He jokes around a lot and likes to make everyone happy through his antics. I consider him a great influence in my life in terms of having a positive outlook and appreciating everything that life has to offer.",
     relation: "Relative",
   },
   {
-    name: "Zhane Dan",
+    name: "Zhane Dan A.",
     description: "Consistent, respectful, and always willing to learn.",
     relation: "College Friend"
   }
