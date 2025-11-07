46df21740b092b1de4891603aab63be4b0c07804
anghelusjr <anghelusjr09.ab@gmail.com>
2025-11-08 01:52:01 +0800

    updated project images on project section in homepage

---
+++ src/pages/homePage/projectSection/projectData.jsx
@@ -0,0 +1,7 @@
+
+export const projectData = [
+    '/projects/ecommerce/ecommerce-main.webp',
+    '/projects/ecommerce/ecommerce-checkout.webp',
+    '/projects/ecommerce/ecommerce-track-order.webp'
+
+]
--- src/pages/homePage/projectSection/projectSection.jsx
+++ src/pages/homePage/projectSection/projectSection.jsx
@@ -12,10 +12,8 @@ export function ProjectSection() {
         <h2 className="text-center text-gray-300 font-medium text-2xl lg:text-3xl xl:text-4xl">Featured Web Design Projects & Development</h2>
         <div className="grid grid-cols max-w-5xl lg:max-w-6xl lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-3 gap-4">
           <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
         </div>
-        <Link to="/projects" className="text-white w-auto h-auto py-3 px-6 bg-purple-600 rounded-md">Check Out my Latest Work</Link>
+        <Link to="/projects" className="text-white w-auto h-auto py-3 px-6 bg-gray-600 rounded-md">Check Out my Latest Work</Link>
       </section>
     </>
   )
