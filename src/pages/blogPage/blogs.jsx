d338ef58b85829ae6d24f834bcb93a27adf3c368
anghelusjr <anghelusjr09.ab@gmail.com>
2026-01-26 12:52:24 +0800

    fix the blog scrolling behavior

--- src/pages/blogPage/blogs.jsx
+++ src/pages/blogPage/blogs.jsx
@@ -10,10 +10,12 @@ function BlogsPage() {
         <meta name="description" content="Read the latest blog posts by Angel Buenavista, a web designer from Taguig, Philippines. Explore insights on web design, development, and digital trends to help you create effective online experiences." />
       </header>
 
-      <article className="flex flex-col items-center h-auto overflow-hidden 
+      <article className="flex flex-col items-center h-auto overflow-scroll
       pb-30 mt-30 px-4 xl:mt-8 lg:ml-16 xl:ml-62 xl:p-10">
-        <h1 className="text-5xl capitalize p-6  bg-gray-800 rounded-3xl text-gray-100 2xl:px-20">Recent Blogs</h1>
-          <BlogCardPage />
+        <h1 className="text-5xl capitalize p-6  bg-gray-800 rounded-3xl text-gray-100 2xl:px-20">
+          Recent Blogs
+        </h1>
+        <BlogCardPage />
       </article >
     </>
   );
